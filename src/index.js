c149087703e8c3713d74583134429843cb432405
constantgillet <constant.gillet@hetic.net>
2020-06-17 11:50:51 +0200

    :pencil: Add todolist and config.php api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './style/main.scss'
 
 console.log('Hello Webpack')
+
+//On demande les satellites depuis l'API
+
+//On applique les mets sur les filtres
+
+//On met les petits points de satellites dans la div
+
+//let filtredSatellites = []
+
+//Filters
+////Si on change les filtres alors on update les objects "Graphiques" ex chartPurposes.update(satellites)
+
+//On gènère les graphiques avec le tableau d'objects satellites
+////Dans les objets lors du survol d'un 

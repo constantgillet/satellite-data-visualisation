36a4fe1f7a8b75649aba874bf3197843a401a39d
aurelbnn <aurel.bonneau@hetic.net>
2020-06-17 13:08:35 +0200

    :sparkles: add webgl earth

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
 import './style/main.scss'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-console.log('Hello Webpack')
+
+/**
+ * Scene
+ */
+const scene = new THREE.Scene()
+
+/**
+ * Sizes
+ */
+const sizes = {}
+sizes.width = window.innerWidth
+sizes.height = window.innerHeight
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 20)
+camera.position.z = 12
+scene.add(camera)
+
+/**
+ * Object
+ */
+const geometry = new THREE.SphereGeometry( 5, 32, 32 );
+const material = new THREE.MeshNormalMaterial();
+const sphere = new THREE.Mesh( geometry, material );
+scene.add( sphere );
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true })
+renderer.setSize(sizes.width, sizes.height)
+document.body.appendChild(renderer.domElement)
+renderer.render(scene, camera)
+
+/**
+ * Camera controls
+ */
+const cameraControls = new OrbitControls(camera, renderer.domElement)
+cameraControls.zoomSpeed = 0.3
+cameraControls.enableDamping = true
+
+/**
+ * Loop
+ */
+const loop = () =>
+{
+    window.requestAnimationFrame(loop)
+
+    cameraControls.update()
+    renderer.render(scene, camera)
+}

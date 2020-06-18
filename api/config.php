569e7a98ba8fab729d319ad960f76885d932725e
constantgillet <constant.gillet@hetic.net>
2020-06-18 12:30:37 +0200

    ✨ Add the API

--- api/config.php
+++ api/config.php
@@ -6,13 +6,10 @@
 //Database configuration
 define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
-define('DB_NAME', 'blog');
+define('DB_NAME', 'satellite_database');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
 $pdo = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASS);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-
-//start session
-session_start();
--- src/index.js
+++ src/index.js
@@ -7,6 +7,15 @@ import Webgl from './scripts/Webgl'
 //On applique les mets sur les filtres
 const filters = new Filters(document.querySelector('.filters'))
 
+fetch('http://satellite-data.local/api/getAllSatellites.php')
+.then(res => res.json())
+.then(data => {
+    console.log(data[0])
+},
+(error) => {     
+console.error(error)       
+})
+
 //On met les petits points de satellites dans la div
 
 //let filtredSatellites = []

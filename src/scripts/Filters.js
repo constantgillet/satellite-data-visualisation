74446ed2fa6d11f7b6f435785464913515338ace
dimitrilenet <dimitri.lenet@hotmail.fr>
2020-06-18 12:56:26 +0200

    :tada:toggle menu

--- src/scripts/Filters.js
+++ src/scripts/Filters.js
@@ -3,17 +3,44 @@ export default class Filters {
         this.element = _element
         this.satellites = satellites
         this.toggleFiltersButton = _element.querySelector('#js-toggle-filters-button')
+        this.filter__button = _element.querySelector('.filters__button')
+        this.filter__button = _element.querySelector('.filters__button')
+        this.filter__button = _element.querySelector('.filters__button')
 
         //Methods
         this.clickToggleFiltersButton()
+        this.clickfilter__button()
     }
 
     clickToggleFiltersButton = () => {
         const form = this.element.querySelector('.filters__form')
 
         this.toggleFiltersButton.addEventListener('click', () => {
-            
+
             form.classList.toggle('active')
         })
     }
-}
+    clickToggleFiltersButton = () => {
+        const form = this.element.querySelector('.filters__button')
+
+        this.toggleFiltersButton.addEventListener('click', () => {
+
+            form.classList.toggle('active')
+        })
+    }
+
+    clickToggleFiltersButton = () => {
+        const form = this.element.querySelector('.filters__option')
+
+        this.toggleFiltersButton.addEventListener('click', () => {
+
+            form.classList.toggle('active')
+        })
+
+        clickToggleFiltersButton = () => {
+            const form = this.element.querySelector('.filters__imput')
+
+            this.toggleFiltersButton.addEventListener('click', () => {
+
+                form.classList.toggle('active')
+            })

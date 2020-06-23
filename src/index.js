987f1c65aaada829a1088c21f2e180397eb534e5
dimitrilenet <dimitri.lenet@hotmail.fr>
2020-06-23 15:56:07 +0200

    :sparkles: caroussel

--- src/index.js
+++ src/index.js
@@ -4,6 +4,45 @@ import Disclamer from './scripts/Disclamer'
 
 const disclamer = new Disclamer()
 
+class partsContent {
+    constructor(element, options = {}) {
+        this.element = element
+        this.options = object.assign({}, {
+            slidesToScroll: 1,
+            sliderVisible: 1
+
+        }, option)
+        this.children = [].slice.call(element.children)
+        let root = this.creatDivWhithClass('caroucel')
+        let container = this.creatDivWhithClass('caroucel__container')
+        root.appendChild(container)
+        this.element.appendChild(root)
+        this.children; array.forEach(function (child) {
+            container.appendChild(child)
+        })
+
+    }
+
+
+    creatDivWhithClass(className) {
+        let div = document.createElement('div')
+        root.setAttribute('class', 'className')
+        return div
+    }
+
+
+
+    document.addEventListener('DOMContentLoad', function() {
+
+        new Carousel(document.querySelector('.part-content'), {
+            sliderVisible: 3
+        })
+    }
+
+}
+
+
+
 let previousButton = document.querySelector('.parts-content__previous-button')
 const step = 960
 let pos = 0
@@ -14,7 +53,6 @@ const printOrbite = document.querySelector('.parts-content__orbite')
 const printLifeDuration = document.querySelector('.parts-content__life-duration')
 const sliderVisualisation = document.querySelector('.part-content')
 const sliderContent = document.querySelectorAll('.part-content .satellite-content')
-
 
 
 // //On ajoute une div à la div .sliderPagination
@@ -28,6 +66,12 @@ nextButton.addEventListener('click', () => {
 
 
     printHistory.classList.toggle('active')
+
+    if (printHistory.classList == ('active')) {
+        add.printHistory.classList.toggle('is-active')
+    }
+
+
 
 })
 
